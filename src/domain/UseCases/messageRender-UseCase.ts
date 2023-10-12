d4571f9bda081038dfa54c5c6b19417d4e86f4ab
quantxz <119700175+quantxz@users.noreply.github.com>
2023-10-12 19:42:16 -0300

    Update messageRender-UseCase.ts

--- src/domain/UseCases/messageRender-UseCase.ts
+++ src/domain/UseCases/messageRender-UseCase.ts
@@ -1,5 +1,5 @@
-export const renderHtml: any = (name: string, publicKey: string, privateKey: string) => {
-  const html = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+export const renderHtml: any = (name: string, publicKey: string, privateKey: string): string => {
+  const html: string = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="font-family:arial, 'helvetica neue', helvetica, sans-serif">
    <head>
     <meta charset="UTF-8">
